f941b381014a5b2291febf1e8e8439f6387c67b6
Victor de Souza Magaldi <victmagaldi@hotmail.com>
2021-01-17 13:53:05 -0300

    limitando a qtd  de digitos no display

--- scripts/controller/calc-controller.js
+++ scripts/controller/calc-controller.js
@@ -324,6 +324,10 @@ class CalcController {
    }
 
    set displayCalc(value) {
+      if (value.toString().length > 10) {
+         this.setError();
+         return false;
+      }
       this._displayCalcEl.innerHTML = value;
    }
 
